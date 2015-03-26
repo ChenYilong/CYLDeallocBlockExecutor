da7f91c2661738ad04cc55a468815d64a2db794a
pifupro_iOS_admin <chenyl@pifupro.com>
2015-03-26 17:27:48 +0800

    Initial Commit

---
+++ LinkTableViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LinkTableViewDemo
+//
+//  Created by chenyilong on 15/3/26.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LinkTableViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LinkTableViewDemo
+//
+//  Created by chenyilong on 15/3/26.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
