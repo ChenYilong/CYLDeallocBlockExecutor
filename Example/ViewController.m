fd346038dc11650167a1cac3a1e3088eca4c0370
wangrui <wangr@qiyi.com>
2016-05-25 10:20:23 +0800

    增加测试用例

--- Example/ViewController.m
+++ Example/ViewController.m
@@ -11,18 +11,49 @@
 #import "CYLDeallocBlockExecutor.h"
 
 @interface ViewController ()
+{
+    NSNumber *_number;
+}
 
 @property (nonatomic, assign, getter=isBySetter) BOOL bySetter;
-
+@property (nonatomic, strong)NSNumber *aNumber;
 @end
 
 @implementation ViewController
+
+- (void)dealloc
+{
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.themeMap = @{ @"BackgroundColor" : @"randomColor" };
     self.bySetter = YES;
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _number = @(5);
+    self.aNumber = @(6);
+    
+    __weak typeof(self) weakSelf = self;
+    __unsafe_unretained typeof(self) unsafeSelf = self;
+    
+    
+    [self cyl_executeAtDealloc:^{
+        
+        __strong typeof(self) strongSelf = weakSelf;
+        
+        //NSNumber *n = strongSelf->_number; //会挂 weakSelf还是具有地址值,strongSelf为nil. (编译器不允许weakSelf->访问指针)
+        
+        NSNumber *n1 = weakSelf.aNumber;
+        NSNumber *n2 = strongSelf.aNumber;
+        NSLog(@"%@,%@",n1,n2); //(null),(null)  尝试读取实例变量,里面的变量均为nil了
+        
+        
+        NSNumber *n3 = unsafeSelf.aNumber; //unsafeSelf也具有地址值
+        NSLog(@"%@",n3);//(null)
+        
+    }];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -31,6 +62,12 @@
         self.view.themeMap = @{ @"BackgroundColor" : @"randomColor" };
     } else {
         [[NSNotificationCenter defaultCenter] postNotificationName:kThemeDidChangeNotification object:nil userInfo:@{ @"BackgroundColor" : @"randomColor" }];
+    }
+    
+    static int i = 0;
+    if (i == 0) {
+        [self.navigationController pushViewController:[ViewController new] animated:YES];
+        i++;
     }
 }
 
